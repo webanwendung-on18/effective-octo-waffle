db5a9f91852d868d75e12c1b8eeedb69fd9e0eb4
Natascha <forsternatascha@gmail.com>
2020-01-29 18:24:08 +0100

    additional styling to profile route

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,20 +8,28 @@ class Profile extends Component {
   render() {
     return (
       <>
-        <h1>Profile</h1>
-        <Link to="/">Home</Link>
-
         <div classNameName="container-fluid">
           <div className="row">
+            <img
+              src="https://coverfiles.alphacoders.com/460/46067.jpg"
+              alt=""
+              className="img img-fluid w-100 "
+            />
+          </div>
+          <div className="row justify-content-center">
             <div className="col-4 col-md-3 ml-4">
               <img
                 src="https://cdn-images-1.medium.com/max/1600/1*zm5NLjdhGd3VVTA2u-xEPg.gif"
                 alt=""
-                className="img img-fluid"
+                className="img img-fluid rounded-circle profilePicture"
               />
             </div>
+          </div>
+          <div className="row justify-content-center">
+            <h4>Name</h4>
+          </div>
+          <div className="row">
             <div className="col-7 ml-auto">
-              <h4>Name</h4>
               <p>
                 # Rezepte hinzugefügt
                 <br />
