cde7784b5c8b095021b13721dcf1365d599f8f29
OleMantei <ole.mantei@gmail.com>
2020-01-30 11:10:31 +0100

    fix Navbar/.container all routes
    Navbar and css class "container" was applied to all sites. Home needs to be different.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Link } from "@reach/router";
+import Navbar from "./Navbar";
 
 class Feed extends Component {
   render() {
     return (
       <>
-        <h1>Feed</h1>
-        <Link to="/">Home</Link>
+        <Navbar />
+        <div className="container">
+          <h1>Feed</h1>
+          <Link to="/">Home</Link>
+        </div>
       </>
     );
   }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "./Navbar";
 
 class Form extends Component {
   render() {
     return (
       <>
-        <h1>Add a recipe</h1>
+        <Navbar />
+        <div className="container">
+          <h1>Add a recipe</h1>
+        </div>
       </>
     );
   }
