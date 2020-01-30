755d705362b323590832cbd8df8b900ec9c267a5
Felix <felixkeller98@gmail.com>
2020-01-30 11:02:38 +0100

    add RecipeCard-Component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "@reach/router";
+import RecipeCard from "./RecipeCard";
 
 class Feed extends Component {
   render() {
     return (
       <>
         <h1>Feed</h1>
-        <Link to="/">Home</Link>
+        <RecipeCard />
       </>
     );
   }
