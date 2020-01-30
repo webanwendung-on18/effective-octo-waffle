6d6cb284856a2e1f6d50bb3d1f85964a68a0617e
Felix <felixkeller98@gmail.com>
2020-01-30 16:34:44 +0100

    add getAllRecipes-Method

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,12 +1,37 @@
 import React, { Component } from "react";
-import { Link } from "@reach/router";
 import RecipeCard from "./RecipeCard";
+import firebase from "./../firebase/config";
+import "firebase/firestore";
+
+var db = firebase.firestore();
 
 class Feed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { recipes: [] };
+  }
+
+  getAllRecipes = async () => {
+    const snapshot = await db.collection("Recipes").get();
+    const allRecipes = [];
+    snapshot.forEach(doc => (allRecipes[doc.id] = doc.data()));
+    this.setState({ recipes: allRecipes });
+    console.log(allRecipes);
+    return this.state.recipes;
+  };
+
+  async componentDidMount() {
+    const allrecipes = await this.getAllRecipes();
+  }
+
   render() {
     return (
       <>
         <h1>Feed</h1>
+        {this.state.recipes.map(recipe => {
+          return <p>{recipe.id}</p>;
+        })}
+        <RecipeCard />
         <RecipeCard />
       </>
     );
--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -3,28 +3,26 @@ import React, { Component } from "react";
 class RecipeCard extends Component {
   render() {
     return (
-      <>
-        <div className="card-container">
-          <div className="card-outline d-flex">
-            <div className="card-body w-40 ">
-              <span className="card-number card-circle subtle">01</span>
-              <span className="card-author subtle">John Smith</span>
-              <h2 className="card-title">New Brunch Recipe</h2>
-              <span className="card-description subtle">
-                These last few weeks I have been working hard on a new brunch
-                recipe for you all.
-              </span>
-              <div className="card-read">Read</div>
-              <span className="card-tag card-circle subtle">C</span>
-            </div>
-            <img
-              src="https://s15.postimg.cc/temvv7u4r/recipe.jpg"
-              alt=""
-              className="card-media img img-fluid"
-            />
+      <div className="card-container">
+        <div className="card-outline d-flex">
+          <div className="card-body w-40 mr-4">
+            <span className="card-number card-circle subtle">01</span>
+            <span className="card-author subtle">John Smith</span>
+            <h2 className="card-title">New Brunch Recipe</h2>
+            <span className="card-description subtle">
+              These last few weeks I have been working hard on a new brunch
+              recipe for you all.
+            </span>
+            <div className="card-read">Cook</div>
+            <span className="card-tag card-circle subtle">C</span>
           </div>
+          <img
+            src="https://s15.postimg.cc/temvv7u4r/recipe.jpg"
+            alt=""
+            className="card-media img img-fluid"
+          />
         </div>
-      </>
+      </div>
     );
   }
 }
