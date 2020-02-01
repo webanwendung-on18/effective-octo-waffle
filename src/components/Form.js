dd2dab02057626a5eb09a679b52ff8e87e673c33
OleMantei <ole.mantei@gmail.com>
2020-02-01 13:45:09 +0100

    fix relocate Navbar
    Strategic change. Navbar is going to be part of all sites.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
-import Navbar from "./Navbar";
 
 class Form extends Component {
   render() {
     return (
       <>
-        <Navbar />
         <div className="container">
           <h1>Add a recipe</h1>
         </div>
