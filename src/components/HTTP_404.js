f28b410a31ea0146d62c0d46dd50a619943a954e
Felix <felixkeller98@gmail.com>
2020-02-03 14:33:33 +0100

    Add user message to 404

--- src/components/HTTP_404.js
+++ src/components/HTTP_404.js
@@ -17,14 +17,24 @@ class HTTP_404 extends Component {
       <div id="http_404">
         <header id="http_404-content">
           <div className="text-center text-light">
-            <h1 className="h1">There's nothing here!</h1>
-            <p className="lead mb-5">
-              Whatever you were looking for doesn't currently exist at this
-              address. Unless you were looking for this error page, in which
-              case: Congrats! You totally found it.
-            </p>
+            <h1 className="h1">
+              404{" "}
+              <span role="img" aria-label="emoji">
+                👀
+              </span>
+            </h1>
+            <h2 className="h2">
+              {this.props.message
+                ? this.props.message
+                : "There's nothing to see here"}
+            </h2>
             <div className="d-flex justify-content-center">
               <div className="w-75">
+                <p className="lead mb-5 mt-2">
+                  Whatever you were looking for doesn't currently exist at this
+                  address. Unless you were looking for this error page, in which
+                  case: Congrats! You totally found it.
+                </p>
                 <Link className="btn btn-primary btn-block mb-3" to="/">
                   Home
                 </Link>
