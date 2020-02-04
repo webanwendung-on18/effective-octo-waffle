a1a353f79e14134e94d90ccadbe7be7413629050
eisenils <mail.eisenils@gmail.com>
2020-02-04 10:55:33 +0100

    Change logo route

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ class Navbar extends Component {
     return (
       <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <Link to="/" className="navbar-brand">
+          <Link to="/recipes" className="navbar-brand">
             Effective-Octo-Waffle
           </Link>
           <button
