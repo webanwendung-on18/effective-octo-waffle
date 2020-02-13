7898f85795cec3039c28b51056d04b38e6d3c033
OleMantei <ole.mantei@gmail.com>
2020-02-13 15:17:25 +0100

    Fix Safari Recipe Card

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -19,37 +19,48 @@ class RecipeCard extends Component {
     } = this.props;
     const recipeIndex = index + 1;
     return (
-      <div className="card-outline d-flex">
-        <div className="card-content mr-4">
-          <span className="card-number card-circle subtle">{("0" + recipeIndex).slice(-2)}</span>
-          <span className="ml-2">
-            {flags.map((flag, idx) => (
-              <span key={idx} className="flag">
-                {flag}
-              </span>
-            ))}
-          </span>
-          <span className="card-author subtle">{name}</span>
-          <h2 className="card-title">
-            <Textfit mode="single">{title}</Textfit>
-          </h2>
-          <span className="card-description subtle">{description}</span>
-          <Link to={`/recipes/${id}`} className="card-read">
-            <span className="underline--magical">Read more</span>
-          </Link>
-          <div className="d-flex flex-wrap align-items-center">
-            <span className="mr-1" style={{ fontWeight: "600" }}>
-              Difficulty:
-            </span>
-            <span className="mr-4">{difficulty}</span>
+      <div className="card-outline">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <div className="card-content mr-4">
+                <span className="card-number card-circle subtle">
+                  {("0" + recipeIndex).slice(-2)}
+                </span>
+                <span className="ml-2">
+                  {flags.map((flag, idx) => (
+                    <span key={idx} className="flag">
+                      {flag}
+                    </span>
+                  ))}
+                </span>
+                <span className="card-author subtle">{name}</span>
+                <h2 className="card-title">
+                  <Textfit mode="single">{title}</Textfit>
+                </h2>
+                <span className="card-description subtle">{description}</span>
+                <Link to={`/recipes/${id}`} className="card-read">
+                  <br></br>
+                  <span className="underline--magical">Read more</span>
+                </Link>
+                <div className="d-flex flex-wrap align-items-center">
+                  <span className="mr-1" style={{ fontWeight: "600" }}>
+                    Difficulty:
+                  </span>
+                  <span className="mr-4">{difficulty}</span>
 
-            <span className="mt-xs-2 mt-lg-0">
-              <IoIosTime size={"1.5em"} color={"#333"} className={"mr-1"} />
-              {`${duration} minutes`}
-            </span>
+                  <span className="mt-xs-2 mt-lg-0">
+                    <IoIosTime size={"1.5em"} color={"#333"} className={"mr-1"} />
+                    {`${duration} minutes`}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-5">
+              <img src={imageUrl} alt="recipeImage" className="card-media" />
+            </div>
           </div>
         </div>
-        <img src={imageUrl} alt="recipeImage" className="card-media" />
       </div>
     );
   }
