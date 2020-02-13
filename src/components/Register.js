f1ecea3c2d8a84150322b2c48d327069bfcb971e
OleMantei <48588093+OleMantei@users.noreply.github.com>
2020-02-13 14:32:45 +0100

    Fix Register.js (#60)

--- src/components/Register.js
+++ src/components/Register.js
@@ -43,10 +43,7 @@ class Register extends Component {
 
     firebase
       .auth()
-      .createUserWithEmailAndPassword(
-        registrationInfo.email,
-        registrationInfo.password
-      )
+      .createUserWithEmailAndPassword(registrationInfo.email, registrationInfo.password)
       .then(() => {
         this.props.registerUser(registrationInfo.userName);
       })
@@ -61,82 +58,82 @@ class Register extends Component {
 
   render() {
     return (
-                        <Helmet>
+      <>
+        <Helmet>
           <title>Register | Octo Waffle</title>
         </Helmet>
-      <div id="home">
-        <div id="home-content">
-          <div className="card card-login">
-            <div className="card-body">
-              <h1 className="h2 text-center mt-2">Register</h1>
-              <h6 className="card-subtitle mb-3 text-muted text-center">
-                Create a new account
-              </h6>
-              <form onSubmit={this.handleSubmit}>
-                {this.state.errorMessage !== null ? (
-                  <FormError theMessage={this.state.errorMessage} />
-                ) : null}
-                <form noValidate autoComplete="off" className="mx-3 mb-1">
-                  <TFPrimary
-                    fullWidth
-                    id="standard-basic"
-                    label="User Name"
-                    className="my-2"
-                    type="text"
-                    name="userName"
-                    value={this.state.userName}
-                    onChange={this.handleChange}
-                  />
-                  <TFPrimary
-                    fullWidth
-                    id="standard-basic"
-                    label="E-Mail"
-                    className="my-2"
-                    type="email"
-                    name="email"
-                    value={this.state.email}
-                    onChange={this.handleChange}
-                  />
-                  <TFPrimary
-                    fullWidth
-                    id="standard-basic"
-                    label="Password"
-                    className="my-2"
-                    type="password"
-                    name="passOne"
-                    value={this.state.passOne}
-                    onChange={this.handleChange}
-                  />
-                  <TFPrimary
-                    fullWidth
-                    id="standard-basic"
-                    label="Repeat Password"
-                    className="my-2"
-                    type="password"
-                    name="passTwo"
-                    value={this.state.passTwo}
-                    onChange={this.handleChange}
-                  />
-                </form>
-                <div className="ml-3 mt-2 mb-5">
-                  <BtnPrimary className="btn btn-primary" type="submit">
-                    Register
-                  </BtnPrimary>
-                </div>
-                <div className="mr-3 d-flex justify-content-center">
-                  <div className="d-flex align-items-end">
-                    <Link to="/login" className="card-link mt-2 text-muted">
-                      Already have an account?
-                    </Link>
+        <div id="home">
+          <div id="home-content">
+            <div className="card card-login">
+              <div className="card-body">
+                <h1 className="h2 text-center mt-2">Register</h1>
+                <h6 className="card-subtitle mb-3 text-muted text-center">Create a new account</h6>
+                <form onSubmit={this.handleSubmit}>
+                  {this.state.errorMessage !== null ? (
+                    <FormError theMessage={this.state.errorMessage} />
+                  ) : null}
+                  <form noValidate autoComplete="off" className="mx-3 mb-1">
+                    <TFPrimary
+                      fullWidth
+                      id="standard-basic"
+                      label="User Name"
+                      className="my-2"
+                      type="text"
+                      name="userName"
+                      value={this.state.userName}
+                      onChange={this.handleChange}
+                    />
+                    <TFPrimary
+                      fullWidth
+                      id="standard-basic"
+                      label="E-Mail"
+                      className="my-2"
+                      type="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange}
+                    />
+                    <TFPrimary
+                      fullWidth
+                      id="standard-basic"
+                      label="Password"
+                      className="my-2"
+                      type="password"
+                      name="passOne"
+                      value={this.state.passOne}
+                      onChange={this.handleChange}
+                    />
+                    <TFPrimary
+                      fullWidth
+                      id="standard-basic"
+                      label="Repeat Password"
+                      className="my-2"
+                      type="password"
+                      name="passTwo"
+                      value={this.state.passTwo}
+                      onChange={this.handleChange}
+                    />
+                  </form>
+                  <div className="ml-3 mt-2 mb-5">
+                    <BtnPrimary className="btn btn-primary" type="submit">
+                      Register
+                    </BtnPrimary>
                   </div>
-                </div>
-              </form>
+                  <div className="mr-3 d-flex justify-content-center">
+                    <div className="d-flex align-items-end">
+                      <Link to="/login" className="card-link mt-2 text-muted">
+                        Already have an account?
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
 
-export default Register
+export default Register;
