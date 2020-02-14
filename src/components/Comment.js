3cda21a0ad8b910f550525bfd70449c8b8120c46
Felix <felixkeller98@gmail.com>
2020-02-14 19:11:40 +0100

    add handleNewCommit

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,29 +1,93 @@
 import React from "react";
-// import Moment from "react-moment";
-import { Link } from "@reach/router";
-import DefaultProfileImg from "../images/profile.jpg";
+import cx from "clsx";
+import { makeStyles } from "@material-ui/styles";
+import Box from "@material-ui/core/Box";
+import Link from "@material-ui/core/Link";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Divider from "@material-ui/core/Divider";
+import Rating from "@material-ui/lab/Rating";
+import ArrowForwardIos from "@material-ui/icons/ArrowForwardIos";
+import Favorite from "@material-ui/icons/Favorite";
+import { usePushingGutterStyles } from "@mui-treasury/styles/gutter/pushing";
+import { useLabelIconStyles } from "@mui-treasury/styles/icon/label";
+import { useRowFlexStyles } from "@mui-treasury/styles/flex/row";
 
-const Comment = ({ date, profileImage, message, username, messageId }) => (
-  <li className="row mb-4 message-area py-2">
-    <div className="col-lg-2 d-flex align-items-center">
-      <img
-        src={profileImage || DefaultProfileImg}
-        alt={username}
-        height="100"
-        width="100"
-        className="timeline-image d-none d-lg-block"
-      />
-    </div>
-    <div className="col-12 col-lg-10">
-      <div className="w-100 d-flex mb-1">
-        <Link className="username" to="/">
-          @{username}
-        </Link>
-        {/* <span className="text-muted ml-auto">{date}</span> */}
-      </div>
-      <p className="message-body">{message}</p>
-    </div>
-  </li>
-);
+const useStyles = makeStyles(({ spacing, palette }) => ({
+  card: {
+    display: "flex",
+    padding: spacing(2),
+    borderRadius: 16
+  },
+  media: {
+    minWidth: "25%",
+    maxWidth: "25%",
+    flexShrink: 0,
+    backgroundColor: palette.grey[200],
+    borderRadius: 12,
+    marginRight: "2rem"
+    // boxShadow: "0 2px 8px 0 #c1c9d7, 0 -2px 8px 0 #cce1e9"
+  },
+  rating: {
+    verticalAlign: "text-top"
+  },
+  content: {
+    padding: spacing(0, 2, 0, 0)
+  },
+  heading: {
+    fontSize: 17,
+    fontWeight: "bold",
+    letterSpacing: "0.5px",
+    marginBottom: 0,
+    marginRight: spacing(1.5),
+    display: "inline-block"
+  },
+  body: {
+    fontSize: 14,
+    color: palette.grey[500]
+  },
+  divider: {
+    margin: spacing(1, 0)
+  },
+  textFooter: {
+    fontSize: 14
+  },
+  icon: {
+    fontSize: "1.2rem",
+    verticalAlign: "bottom"
+  }
+}));
+
+const Comment = ({ username, message, profileImage }) => {
+  const styles = useStyles();
+  const gutterStyles = usePushingGutterStyles({ space: 1.5 });
+  const labelStyles = useLabelIconStyles({ linked: true });
+  const flexStyles = useRowFlexStyles();
+  return (
+    <Card className={styles.card} variant="outlined">
+      <CardMedia className={styles.media} image={profileImage} />
+      <CardContent className={styles.content}>
+        <Box mb={1}>
+          <h3 className={styles.heading}>{username}</h3>
+          <Rating name={"rating"} value={2} className={styles.rating} size={"small"} />
+        </Box>
+        <p className={styles.body}>{message}</p>
+        <Divider className={styles.divider} light />
+        <div className={flexStyles.parent}>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <Link className={cx(labelStyles.primaryLink, styles.textFooter)} component={"button"}>
+            Read more <ArrowForwardIos className={labelStyles.icon} />
+          </Link>
+          <div className={cx(flexStyles.rightChild, flexStyles.parent, gutterStyles.parent)}>
+            <button type={"button"} className={labelStyles.link}>
+              <Favorite className={labelStyles.icon} /> 12
+            </button>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default Comment;
--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import CommentMui from "./CommentMui";
+import Comment from "./Comment";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
 
 import firebase from "./../firebase/config";
 import "firebase/firestore";
-import { Grid, TextField } from "@material-ui/core";
+import { Grid, TextField, Button } from "@material-ui/core";
 var db = firebase.firestore();
 
 class CommentList extends Component {
   constructor(props) {
     super(props);
-    this.state = { comments: [], loading: false };
+    this.state = { comments: [], loading: false, comment: "", snackbarOpen: false };
+    this.handleNewComment = this.handleNewComment.bind(this);
   }
   async componentDidMount() {
     try {
@@ -17,7 +20,6 @@ class CommentList extends Component {
       db.collection(`Recipes/${this.props.recipeId}/Comments`).onSnapshot(snapshot => {
         let comments = [];
         snapshot.forEach(doc => comments.push({ ...doc.data(), id: doc.id }));
-        console.log("fdsaasdfas", comments);
         if (comments.length > 0) {
           this.setState({ comments, loading: false });
         } else {
@@ -29,37 +31,91 @@ class CommentList extends Component {
     }
   }
 
+  handleNewComment = e => {
+    e.preventDefault();
+    console.log("sdafasf");
+    const { comment } = this.state;
+    const { user } = this.props;
+    if (this.state.comment === "") {
+      this.setState({ snackbarOpen: true });
+      return;
+    }
+
+    db.collection(`Recipes/${this.props.recipeId}/Comments`)
+      .add({
+        comment,
+        user_id: user.uid,
+        user_name: user.displayName
+      })
+      .catch(err => {
+        console.log(`Error adding document: ${err}`);
+      });
+    return;
+  };
+
+  handleChange = e => {
+    this.setState(
+      {
+        [e.target.name]: e.target.value
+      },
+      () => console.log(this.state)
+    );
+  };
   render() {
     return (
-      <div className="col-10">
-        <ul className="list-group" id="messages">
-          {this.state.comments.length > 0 &&
-            this.state.comments.map((comment, idx) => {
-              console.log("comment", comment);
-              return (
-                <CommentMui
-                  key={idx}
-                  date={comment.date}
-                  message={comment.message}
-                  messageId={comment.id}
-                  username={comment.user_name}
-                  profileImage={comment.profileImageUrl}
+      <>
+        <Snackbar open={this.state.snackbarOpen} autoHideDuration={6000} onClose={this.handleClose}>
+          <MuiAlert elevation={6} variant="filled" onClose={this.handleClose} severity="error">
+            Some required fields are empty
+          </MuiAlert>
+        </Snackbar>
+        <Grid container>
+          <Grid item xs={12} md={8}>
+            {this.state.comments.length > 0 &&
+              this.state.comments.map((comment, idx) => {
+                console.log("comment", comment);
+                return (
+                  <Comment
+                    key={idx}
+                    date={comment.date}
+                    message={comment.message}
+                    messageId={comment.id}
+                    username={comment.user_name}
+                    profileImage={comment.profileImageUrl}
+                  />
+                );
+              })}
+          </Grid>
+          <Grid item xs={12} className="mt-4">
+            <form onChange={this.handleChange} className="d-flex align-items-baseline">
+              <Grid item xs={10} md={7} className="align-self-end">
+                <TextField
+                  fullWidth
+                  required
+                  label="Leave a comment"
+                  name="comment"
+                  multiline
+                  rowsMax="3"
+                  value={this.state.comment}
                 />
-              );
-            })}
-        </ul>
-        <Grid item xs={10}>
-          <TextField
-            required
-            label="Leave a comment"
-            name="description"
-            multiline
-            rowsMax="3"
-            value={this.props.description}
-            fullWidth
-          />
+              </Grid>
+              <Grid item xs={2} className="d-flex ">
+                <Button
+                  onClick={this.handleNewComment}
+                  variant="contained"
+                  color="primary"
+                  className="mt-4 ml-2 align-self-end"
+                >
+                  {"Submit"}
+                </Button>
+              </Grid>
+            </form>
+          </Grid>
         </Grid>
-      </div>
+        {/* <div className="col-10"> */}
+
+        {/* </div> */}
+      </>
     );
   }
 }
